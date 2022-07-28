d26d6c90ca91962f44be91a5364787f8020c58d6
patsicko <patsicko@g>
2022-07-28 21:52:30 +0200

    F

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+
+
 const container=document.getElementById("container");
 
 const fetchData=async()=>{
-   const data= await (await fetch("https://mdbackend.herokuapp.com/api/authenticate",{method:"POST"})).json();
+   const data= await (await fetch("https://mdbackend.herokuapp.com/api/hospitals")).json();
 
    console.log(data);
+
+   for(item in data){
+      (`${item}:${data[item]}`)
+      console.log(item.map(e=>e))
+   }
 }
 
 fetchData();
